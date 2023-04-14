bd79939dca118fa06dc82a52892ccf7eaa7504fc
Murilo Correia <murilo.correiab@gmail.com>
2023-04-14 01:27:19 -0400

    feat: Create project's basic structure

---
+++ src/main/java/com/justeattakeaway/courierstatement/CourierStatementApplication.java
@@ -0,0 +1,13 @@
+package com.justeattakeaway.courierstatement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourierStatementApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CourierStatementApplication.class, args);
+  }
+
+}
