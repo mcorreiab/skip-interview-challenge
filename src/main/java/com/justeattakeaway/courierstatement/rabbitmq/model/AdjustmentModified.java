4ae7526092c3899dd0798f0a335d7f4f8b0123df
Murilo Correia <murilo.correiab@gmail.com>
2023-04-14 22:19:23 -0400

    feat: Add validation to adjustment modified message

--- src/main/java/com/justeattakeaway/courierstatement/rabbitmq/model/AdjustmentModified.java
+++ src/main/java/com/justeattakeaway/courierstatement/rabbitmq/model/AdjustmentModified.java
@@ -1,7 +1,13 @@
 package com.justeattakeaway.courierstatement.rabbitmq.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-public record AdjustmentModified(String adjustmentId, String deliveryId, LocalDateTime modifiedTimestamp, BigDecimal value) {
+public record AdjustmentModified(
+    @NotBlank String adjustmentId,
+    @NotBlank String deliveryId,
+    @NotNull LocalDateTime modifiedTimestamp,
+    @NotNull BigDecimal value) {
 }
