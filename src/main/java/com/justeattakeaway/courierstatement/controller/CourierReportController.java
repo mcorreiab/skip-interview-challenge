d5ab25aff3629548015ca1bf1d047cec0375dc56
Murilo Correia <murilo.correiab@gmail.com>
2023-04-15 19:56:34 -0400

    feat: Create the controller to get the weekly statement

--- src/main/java/com/justeattakeaway/courierstatement/controller/CourierReportController.java
+++ src/main/java/com/justeattakeaway/courierstatement/controller/CourierReportController.java
@@ -1,7 +1,10 @@
 package com.justeattakeaway.courierstatement.controller;
 
 import com.justeattakeaway.courierstatement.usecase.DeliveryReportUseCase;
+import com.justeattakeaway.courierstatement.usecase.StatementUseCase;
 import com.justeattakeaway.courierstatement.usecase.model.DeliveryReport;
+import com.justeattakeaway.courierstatement.usecase.model.WeeklyStatement;
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,21 +12,45 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/couriers/{courierId}")
 public class CourierReportController {
 
   final DeliveryReportUseCase deliveryReportUseCase;
+  final StatementUseCase statementUseCase;
 
-  public CourierReportController(DeliveryReportUseCase deliveryReportUseCase) {
+  public CourierReportController(DeliveryReportUseCase deliveryReportUseCase,
+                                 StatementUseCase statementUseCase) {
     this.deliveryReportUseCase = deliveryReportUseCase;
+    this.statementUseCase = statementUseCase;
   }
 
-  @GetMapping(value = "/couriers/{courierId}/report/from/{from}/to/{to}")
+  @GetMapping(value = "/report/from/{from}/to/{to}")
   public ResponseEntity<Page<DeliveryReport>> getCourierReport(@PathVariable String courierId,
                                                                @PathVariable LocalDate from,
                                                                @PathVariable LocalDate to,
                                                                Pageable pageable) {
-    return ResponseEntity.ok(deliveryReportUseCase.findByCourierAndPeriod(courierId, from, to, pageable));
+    final var reports =
+        deliveryReportUseCase.findByCourierAndPeriod(courierId, from, to, pageable);
+
+    if (reports.isEmpty()) {
+      return ResponseEntity.notFound().build();
+    }
+
+    return ResponseEntity.ok(reports);
+  }
+
+  @GetMapping(value = "/statement/week/{date}")
+  public ResponseEntity<WeeklyStatement> getWeeklyStatement(@PathVariable String courierId,
+                                                            @PathVariable LocalDate date) {
+    final var statement = statementUseCase.getWeeklyStatementByCourierAndDate(courierId, date);
+
+    if (statement.totalAmount().equals(BigDecimal.ZERO)) {
+      return ResponseEntity.notFound().build();
+    }
+
+    return ResponseEntity.ok(statement);
   }
 }
