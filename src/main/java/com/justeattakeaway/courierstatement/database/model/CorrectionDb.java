a461dd2aefd787fc7203bc9dd25321d8863ea382
Murilo Correia <murilo.correiab@gmail.com>
2023-04-15 21:09:26 -0400

    refactor: Rename correction table to be corrections

--- src/main/java/com/justeattakeaway/courierstatement/database/model/CorrectionDb.java
+++ src/main/java/com/justeattakeaway/courierstatement/database/model/CorrectionDb.java
@@ -11,7 +11,7 @@ import jakarta.persistence.ManyToOne;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-@Entity
+@Entity(name = "corrections")
 public class CorrectionDb {
   @Id
   String id;
